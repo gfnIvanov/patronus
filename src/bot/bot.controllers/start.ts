705588100b835221981d495b6a7ee654c3c6b520
Игорь Иванов <is.ivanov@bars.group>
2023-01-16 18:04:46 +0300

    visitor anonimous

--- src/bot/bot.controllers/start.ts
+++ src/bot/bot.controllers/start.ts
@@ -10,7 +10,7 @@ export async function start(
         Update.MessageUpdate<Message>
     >,
 ) {
-    setVisitor(ctx.from.username as string);
+    setVisitor(ctx.from.username);
     await ctx.reply(getGreeting(), {
         ...getKeyboard('greet'),
     });
--- src/database/database.hooks/setVisitor.ts
+++ src/database/database.hooks/setVisitor.ts
@@ -1,7 +1,11 @@
 import { db } from '../database';
 import type { TVisitors } from '../database.types';
 
-export function setVisitor(username: string) {
+export function setVisitor(username: string | undefined) {
+    if (!username) {
+        db.visitors.insert({ username: 'Анонимный волшебник' });
+        return;
+    }
     db.visitors.find({ username }, (err: Error, data: TVisitors[]) => {
         if (err) throw new Error('Ошибка при получении данных');
         if (data.length === 0) {
